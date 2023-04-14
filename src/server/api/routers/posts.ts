1ad4dc7fb1d9fdd94a1725b33392a6e78c8f866e
c0descrub <jordanjames1095@googlemail.com>
2023-04-14 16:08:32 +0100

    Added SSG helpers to profile page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
@@ -10,14 +9,6 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    userName: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
@@ -59,7 +50,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji("Only emojis are allowed").min(1).max(255),
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
       })
     )
     .mutation(async ({ ctx, input }) => {
