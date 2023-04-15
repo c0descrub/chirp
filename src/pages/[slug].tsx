422411cb696be09dfcf7d8893532f72045cf459c
c0descrub <jordanjames1095@googlemail.com>
2023-04-15 14:15:27 +0100

    created pagelayout component, styling profile page.

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -5,6 +5,8 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import { api } from "~/utils/api";
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -17,9 +19,22 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       <Head>
         <title>{data.userName}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <h1>{data.userName}</h1>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 border-slate-400 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`@${data.userName ?? ""}'s profile picture`}
+            width={150}
+            height={150}
+            className="absolute bottom-0 left-0 -mb-[75px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[75px]"></div>
+        <div className="p-4 text-2xl font-semibold">{`@${
+          data.userName ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
 };
