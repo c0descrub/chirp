92f13f1e6b2261a3eab398fa5905bbcfef3fa45a
c0descrub <jordanjames1095@googlemail.com>
2023-04-15 19:53:05 +0100

    extended link to profile to include profile pic

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import type { RouterOutputs } from "~/utils/api";
@@ -9,13 +8,15 @@ export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div className="flex gap-3 border-b border-slate-400 p-4" key={post.id}>
-      <Image
-        alt={`@${author.username}'s profile picture`}
-        className="h-14 w-14 rounded-full"
-        src={author.profileImageUrl}
-        width={56}
-        height={56}
-      />
+      <Link href={`/@${author.username}`}>
+        <Image
+          alt={`@${author.username}'s profile picture`}
+          className="h-14 w-14 rounded-full"
+          src={author.profileImageUrl}
+          width={56}
+          height={56}
+        />
+      </Link>
       <div className="flex flex-col">
         <div className="flex gap-1  text-slate-300">
           <Link href={`/@${author.username}`}>
